8e23e83a15a6386fbf426a00a5089e4821fef8a4
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2016-10-29 15:08:00 -0400

    Adds finished run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,15 @@
 # I think that I need these libraries
+library(reshape2)
 library(data.table)
 library(plyr)
 library(tidyr)
 library(dplyr)
+
+# Check to see if the UCI HAR Dataset is in the working directory. If not, download it.
+if (!dir.exists('UCI HAR Dataset')) {
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "UCI HAR Dataset.zip")
+  unzip('UCI HAR Dataset.zip')
+}
 
 # Read in the data that applies to both the train and test data sets
 features <- read.table('UCI HAR Dataset/features.txt')
@@ -18,16 +25,12 @@ test_x <- read.table('UCI HAR Dataset/test/X_test.txt')
 test_y <- read.table('UCI HAR Dataset/test/Y_test.txt')
 
 # Set the dataset column names to be the values in the list of features
+# This is Step 4: "Appropriately label the data set with descriptive variable names"
 names(train_x) <- features$V2
 names(test_x) <- features$V2
 
 # We don't need the feature data anymore, so release that memory
 rm('features')
-
-# Add a column to each set of feature vectors to denote whether the data came from the train or test set
-# This is done so that when the sets are combined, they can still be differentiated
-train_x$dataset <- "train"
-test_x$dataset <- "test"
 
 # Set the column name of the labels data sets (train_y and test_y) to "activity_label" to provide more description
 names(train_y) <- c("activity_label")
@@ -70,5 +73,18 @@ rm('train_x')
 rm('test_x')
 
 # "Extracts only the measurements on the mean and standard deviation for each measurement" (Step 2)
-# Do something about the repeated column names
-activity_data <- select(activity_data, grep("[Mm]ean|std|dataset|activity_label|subject_id", names(activity_data)))
+columns_i_want <- grep("[Mm]ean|std|dataset|activity_label|subject_id", names(activity_data))
+activity_data <- activity_data[, columns_i_want]
+rm('columns_i_want')
+
+# Create a new data set that contains the means of the extracted features grouped by subject and activity (Step 5: First part)
+tidy_data <- aggregate(activity_data[,1:86], 
+                       by = list(subject_id = activity_data$subject_id, activity_label = activity_data$activity_label), 
+                       FUN = mean)
+
+# We don't need the activity_data dataframe anymore, so let's release that memory
+rm('activity_data')
+
+# Makes the data set narrow and tidy (Finishes Step 5)
+tidy_data <- melt(tidy_data, id = c("subject_id", "activity_label"), variable.name = "feature", value.name = "mean")
+write.table(tidy_data, "tidy_data.txt")
